4fc3c4c6023c3d0022fe6b4a567adedcc27fb380
James Tan <james_tan97@outlook.com>
2022-06-14 00:52:04 -0400

    Add lists

--- bin/main.ml
+++ bin/main.ml
@@ -4,9 +4,15 @@ type stream = {
   chan : in_channel;
 }
 
-type lobject = Fixnum of int | Boolean of bool | Symbol of string
+type lobject =
+  | Fixnum of int
+  | Boolean of bool
+  | Symbol of string
+  | Nil
+  | Pair of lobject * lobject
 
 exception SyntaxError of string
+exception ThisCan'tHappenError
 
 let read_char stm =
   match stm.chr with
@@ -30,7 +36,7 @@ let rec eat_whitespace stm =
 
 let string_of_char c = String.make 1 c
 
-let read_sexp stm =
+let rec read_sexp stm =
   let is_digit c =
     let code = Char.code c in
     code >= Char.code '0' && code <= Char.code '9'
@@ -64,11 +70,22 @@ let read_sexp stm =
       ""
     else string_of_char nc ^ read_symbol ()
   in
+  let rec read_list stm =
+    eat_whitespace stm;
+    let c = read_char stm in
+    if c = ')' then Nil
+    else
+      let _ = unread_char stm c in
+      let car = read_sexp stm in
+      let cdr = read_list stm in
+      Pair (car, cdr)
+  in
   eat_whitespace stm;
   let c = read_char stm in
   if start_char_is_sym c then Symbol (string_of_char c ^ read_symbol ())
   else if is_digit c || c = '~' then
-    read_fixnum (Char.escaped (if c = '~' then '-' else c))
+    read_fixnum (string_of_char (if c = '~' then '-' else c))
+  else if c = '(' then read_list stm
   else if c = '#' then
     match read_char stm with
     | 't' -> Boolean true
@@ -76,11 +93,36 @@ let read_sexp stm =
     | x -> raise (SyntaxError ("Invalid boolean literal " ^ Char.escaped x))
   else raise (SyntaxError ("Unexpected char " ^ Char.escaped c))
 
-let print_sexp e =
+let rec print_sexp e =
+  let rec is_list e =
+    match e with Nil -> true | Pair (_, b) -> is_list b | _ -> false
+  in
+  let rec print_list l =
+    match l with
+    | Pair (a, Nil) -> print_sexp a
+    | Pair (a, b) ->
+        print_sexp a;
+        print_string " ";
+        print_list b
+    | _ -> raise ThisCan'tHappenError
+  in
+  let print_pair p =
+    match p with
+    | Pair (a, b) ->
+        print_sexp a;
+        print_string " ";
+        print_sexp b
+    | _ -> raise ThisCan'tHappenError
+  in
   match e with
   | Fixnum v -> print_int v
   | Boolean b -> print_string (if b then "#t" else "#f")
   | Symbol s -> print_string s
+  | Nil -> print_string "nil"
+  | Pair (_, _) ->
+      print_string "(";
+      if is_list e then print_list e else print_pair e;
+      print_string ")"
 
 let rec repl stm =
   print_string "> ";
